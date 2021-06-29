deb99d50e4486c550233f0797747c43f840b1c15
yesiney <yeniseys16@gmail.com>
2021-06-29 15:43:28 +0300

    php challenge - 2 is finished

--- process.php
+++ process.php
@@ -4,44 +4,59 @@
 <body>
 
     <?php
-    $myfile = fopen("./input/input.txt", "r") or die("Unable to open file!");
+
+    $turkish = array("ı", "ğ", "ü", "ş", "ö", "ç", "Ç", "Ş", "İ", "Ü", "Ö"); //turkish letters
+    $english   = array("i", "g", "u", "s", "o", "c", "C", "S", "I", "U", "O"); //english cooridinators letters
+
+    $myfile = fopen(__DIR__ . "./input/input.txt", "r") or die("Unable to open file!");
 
     $xml = new XMLWriter;
-    $xml->openURI('file:///C:/xampp/htdocs/PHP-Challenge/output/output.xml');
-    $xml->setIndent(true); // makes output cleaner
+    $xml->openURI(__DIR__ . './output/output.xml');
+    $xml->setIndent(true);
 
     $xml->startElement('order');
     $xml->startElement('header');
 
-    $line = fgetcsv($myfile, 100,";");
+    $line = fgetcsv($myfile, 100, ";");
 
-    $data = fgetcsv($myfile, 100,";");
-    
+    $data = fgetcsv($myfile, 100, ";");
+
     $xml->writeElement($line[0], $data[0]);
     $xml->writeElement($line[1], $data[1]);
-    $xml->writeElement($line[2], $data[2]);
-    $xml->writeElement($line[3], $data[3]);
+    $date = new DateTime($data[2]);
+    $dateToSave = $date->format('Y-m-d H:i:s');
+    $xml->writeElement($line[2], $dateToSave);
+    $date = new DateTime($data[3]);
+    $dateToSave = $date->format('Y-m-d H:i:s');
+    $xml->writeElement($line[3], $dateToSave);
     $xml->writeElement($line[4], $data[4]);
     $xml->endElement();
 
-    $line = fgetcsv($myfile, 1000,";");
-    
+    $line = fgetcsv($myfile, 1000, ";");
+
     $xml->startElement('lines');
-    // Convert each line into the local $data variable
+
     while ($data = fgetcsv($myfile, 100, ";")) {
-        // Read the data from a single line
+
+        $checkNumber = substr($data[3], 1, 1);
+
+        if (empty($data[1]) || $checkNumber != "," && $checkNumber != "") {
+            continue;
+        }
+
         $xml->startElement('line');
-        
         $xml->writeElement($line[0], $data[0]);
         $xml->writeElement($line[1], $data[1]);
-        $xml->writeElement($line[2], $data[2]);
+        $final_title = str_replace($turkish, $english, $data[2]);
+        $xml->writeElement($line[2], $final_title);
         $xml->writeElement($line[3], $data[3]);
         $xml->writeElement($line[4], $data[4]);
         $xml->writeElement($line[5], $data[5]);
         $xml->writeElement($line[6], $data[6]);
-        $xml->writeElement($line[7], $data[7]);
+        $date = new DateTime($data[7]);
+        $dateToSave = $date->format('ymd');
+        $xml->writeElement($line[7], $dateToSave);
         $xml->endElement();
-
     }
 
     $xml->endElement();
